490f3bef4a3a66884eba45da9409479f38965f23
kovecu <cristian8661@gmail.com>
2022-11-02 11:44:35 -0300

    errores en form

--- src/app/modules/components/form-register/form-register.component.ts
+++ src/app/modules/components/form-register/form-register.component.ts
@@ -19,13 +19,14 @@ export class FormRegisterComponent implements OnInit {
   }
   myForm(){
     this.registerForm = this.fb.group({
-      name: ['', Validators.required],
-      pLastName: ['', Validators.required],
-      mLastName: ['', Validators.required],
+      name: ['', Validators.compose([Validators.pattern('^[a-zA-Z]+$'), Validators.required])],
+      pLastName: ['', Validators.compose([Validators.pattern('^[a-zA-Z]+$'), Validators.required])],
+      mLastName: ['', Validators.compose([Validators.pattern('^[a-zA-Z]+$'), Validators.required])],
       fBorn: ['', Validators.required ],
       pEmail: ['', Validators.compose([Validators.pattern('^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$'), Validators.required])],
       // pEmail: ['', Validators.email],
-      cPhone: ['', Validators.required],
+      // cPhone: ['', Validators.compose([Validators.pattern('^(+56[-]*(9)[0-9][-]*){8}+$'), Validators.required])],
+      cPhone: ['', Validators.required ],
       landLine:['', Validators.required ],
       street: ['', Validators.required],
       eNumber: [null, Validators.required],
@@ -37,12 +38,27 @@ export class FormRegisterComponent implements OnInit {
     });
     
   }
+
+  form_validation_message = {
+    name: [
+      {type: 'required', message: 'Nombre es requerido'},
+      {type: 'minlength', message: 'el nombre debe contener minimo 5 caracteres'},
+      {type: 'maxlength', message: 'el nombre debe contener maximo 15 caracteres'},
+      {type: 'pattern', message: 'tu nombre solo debe contener letras'}
+    ],
+    pEmail: [
+      {type: 'required', message: 'el email es requerido'},
+      {type: 'pattern', message: 'ingresa un email valido'}
+    ]
+  }
+ 
 //
   onSubmit(){
     this.submited =true;
     if(this.registerForm.valid){
       alert('énvio exitoso');
       return console.table(this.registerForm.value);
+      
     }
   }
 }
